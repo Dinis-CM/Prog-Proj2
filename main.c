7affdec023965d76c28ee439cbede8f000c3e6a7
Dinis-CM <119040954+Dinis-CM@users.noreply.github.com>
2024-03-30 12:41:16 +0000

    Comentários

--- main.c
+++ main.c
@@ -7,23 +7,25 @@
 
 int main(int argc, char *argv[])
 {
- /*Inicializção das listas de aeroportos, rotas e escalas*/
+	//Inicializção das listas de aeroportos, rotas e escalas
     ListaAero *topo_aero = NULL;
-
     ListaRotas *topo_rotas = NULL;
-
     ListaEscalas *topo_escalas = NULL;
-    
+	
+    //Leitura dos ficheiros
     leitura("aeroportos.txt", &topo_aero, &topo_rotas);
     leitura("rotas.txt", &topo_aero, &topo_rotas);       
-    
+
+	//Possiveis pedidos do utilizador
     switch(argc)
     {
         case 2:
+			//Lista de aeroportos
             if(strcmp(argv[1], "-aeroportos")==0){
                 Mostra_lista_aero(topo_aero);
                 return 0;        
             }
+			//Lista de rotas	
             else if(strcmp(argv[1], "-voos")==0){ 
                 Mostra_lista_rotas(topo_rotas);  
                 return 0;
@@ -31,6 +33,7 @@ int main(int argc, char *argv[])
             break;
 
         case 5:
+			//Lista de voos com escalas
             if(strcmp(argv[3], "-L")==0 && strlen(argv[1])==3 && strlen(argv[2])==3 && strlen(argv[4])==1){
                 topo_escalas = Seleciona_lista(topo_rotas, argv[1], argv[2], argv[4]);
                 Mostra_lista_escalas(topo_escalas, argv[4]);
@@ -38,6 +41,7 @@ int main(int argc, char *argv[])
             }
             break;
 		case 6:
+			//Lista de voos com escala e ordenacao por ordem de partida ou selecao distância mais curta
             if(strcmp(argv[3], "-L")==0 && strlen(argv[1])==3 && strcmp(argv[1], argv[2])!=0 && strlen(argv[2])==3 && strlen(argv[4])==1 && ( strlen(argv[5])==2 || strlen(argv[5])==3)){
                 topo_escalas = Seleciona_lista(topo_rotas, argv[1], argv[2], argv[4]);
                 Ordena_lista(&topo_escalas, argv[5]);                
@@ -47,6 +51,7 @@ int main(int argc, char *argv[])
             break;
 
         case 7:
+			//Lista de voos com escala e ordenacaoo por ordem de partida e selecao por distância mais curta
             if(strcmp(argv[3], "-L")==0 && strlen(argv[1])==3 && strcmp(argv[1], argv[2])!=0 && strlen(argv[2])==3 && strlen(argv[4])==1 && ( strlen(argv[5])==2 || strlen(argv[5])==3) && ( strlen(argv[6])==2 || strlen(argv[6])==3)){
                 topo_escalas = Seleciona_lista(topo_rotas, argv[1], argv[2], argv[4]);
                 Ordena_lista(&topo_escalas, argv[5]);
@@ -56,6 +61,7 @@ int main(int argc, char *argv[])
             }
             break;
     }
+	//Leitura dos argumentos do utilizador invalida
     printf("Parâmetros inválidos \n");
     return 1;
 }
